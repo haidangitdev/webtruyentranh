488d6922a179fc30cca72885fe191348c5d6c377
haidangitdev <haidangitdev@gmail.com>
2024-03-21 21:31:19 +0700

    Dựng khung layout mặc định

--- src/components/Layout/DefaultLayout/Silebar/index.js
+++ src/components/Layout/DefaultLayout/Silebar/index.js
@@ -1,4 +1,11 @@
+import classNames from "classnames/bind";
+import styles from './Silebar.module.scss'
+
+const cx = classNames.bind(styles);
+
 function Silebar() {
-    return <h2>Silebar</h2>;
+    return <aside className={cx('wrapper')}>
+        <h2>Silebar</h2>
+    </aside>;
 }
 export default Silebar;
